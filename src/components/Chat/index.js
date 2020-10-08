ab31ede21f3055bf0943fcfee2bcdee3efe9e799
Moises Sanabria <sesmoi.data@gmail.com>
2020-10-08 06:56:31 -0400

    ReactRouter Link Chat func comp importing Avatar, prop args name, message, profilePic, timestamp

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -1,30 +1,25 @@
-import * as React from 'react'
-import PropTypes from 'prop-types'
-import Avatar from '@material-ui/core/Avatar'
-import { Link } from 'react-router-dom'
+import PropTypes from "prop-types";
+import Avatar from "@material-ui/core/Avatar";
+import { Link } from "react-router-dom";
 
-export default function Chat ({ name, message, profilePic, timestamp }) {
+export default function Chat({ name, message, profilePic, timestamp }) {
   return (
     <Link to={`/chat/${name}`}>
-      <div className='chat'>
-        <Avatar
-          className='chat-image'
-          alt={name}
-          src={profilePic}
-        />
-        <div className='chat-details'>
+      <div className="chat">
+        <Avatar className="chat-image" alt={name} src={profilePic} />
+        <div className="chat-details">
           <h2>{name}</h2>
           <p>{message}</p>
+          <p>{timestamp}</p>
         </div>
       </div>
     </Link>
-  )
+  );
 }
 
 Chat.propTypes = {
   name: PropTypes.string.isRequired,
   message: PropTypes.string.isRequired,
   profilePic: PropTypes.string.isRequired,
-  timestamp: PropTypes.string.isRequired
-}
-
+  timestamp: PropTypes.string.isRequired,
+};
